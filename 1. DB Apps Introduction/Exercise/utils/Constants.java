4b0cd6383c841313f14c027c33b5049db3113915
Doichin251002 <dodi_hristov@abv.bg>
2024-02-15 17:51:43 +0200

    Solutions of problems in Theme: DB Apps Introduction
    • Application to Database Connection;
    • JDBC Statements;
    • SQL Injection;
    • Advanced Concepts.

---
+++ 1. DB Apps Introduction/Exercise/utils/Constants.java
@@ -0,0 +1,12 @@
+package utils;
+
+public class Constants {
+    public static final String USER_KEY = "user";
+    public static final String USER_VALUE = "root";
+    public static final String PASSWORD_KEY = "password";
+    public static final String PASSWORD_VALUE = "dodi";
+    public static final String COLUMN_NAME = "name";
+    public static final String COLUMN_AGE = "age";
+
+    public static final String JDBC_MYSQL_URL = "jdbc:mysql://localhost:3306/minions_db";
+}
